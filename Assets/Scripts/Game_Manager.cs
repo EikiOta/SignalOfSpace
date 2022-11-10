947cd1de18a43d1ab276b44052090a129912d128
Hiroki-Imai <ceneca15723@gmail.com>
2022-11-11 02:15:49 +0900

    敵を倒すとスコアが加算される仕様を追加

---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game_Manager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText; //スコア用のテキストを入れる変数
+    private int score; //スコアを入れる変数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //スコアを更新する関数
+    public void UpdateScore(int addScore)
+    {
+        score += addScore; //スコアを"addScore"の値分増やす
+        scoreText.text = "SCORE :" + score; //スコアテキストを更新
+    }
+}
